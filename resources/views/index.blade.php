4ea1062651d4eb045fc52dda7c7477d3fa810e48
Ahmedf <ahmedfaisalzayed@gmail.com>
2025-04-24 19:39:00 +0200

    another commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
-  </head>
-  <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">Blogs</span>
-        </div>
-    </nav>
+@extends('layouts.app')
+@section('title') Home @endsection
+@section('content')
       <div class="d-flex justify-content-center p-3">
         <button class="btn btn-success " type="button">Create Post</button>
       </div>
@@ -41,6 +30,4 @@
     @endforeach
   </tbody>
 </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
-  </body>
-</html>
+@endsection
