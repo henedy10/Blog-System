7af527d65d32c1e88786759273dfaa07e671396e
Ahmedf <ahmedfaisalzayed288@gmail.com>
2025-05-10 15:30:08 +0300

    another commit

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-// Route::get('/',function(){
-//     return view('posts.welcome');
-// });
 
-Route::get('/posts',[PostController::class,'index'])->name('posts.index');
+Route::get('/',[PostController::class,'index'])->name('posts.index');
 Route::get('/posts/create',[PostController::class,'create'])->name('posts.create');
 Route::post('/posts',[PostController::class,'store'])->name('posts.store');
 Route::put('/posts/{post}',[PostController::class,'update'])->name('posts.update');
