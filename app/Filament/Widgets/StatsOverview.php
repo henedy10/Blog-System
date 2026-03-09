a4a2bb10938d1b6b3201e3a59795a1a4161d290f
Ahmedf <ahmedfaisalzayed288@gmail.com>
2026-03-09 07:18:23 +0200

    handling statsOverview widgets

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\Post;
-use App\Models\User;
+use App\Models\
+{
+    Post,
+    User
+};
+use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -11,10 +15,55 @@ class StatsOverview extends StatsOverviewWidget
 {
     protected function getStats(): array
     {
+        // Post Statics
+        $PostsThisMonth = Post::whereMonth('created_at',Carbon::now())->count();
+        $PostsLastMonth = Post::whereMonth('created_at',Carbon::now()->subMonth())->count();
+        for($i=6;$i>=0;$i--){
+            $PostsChart[6-$i] = Post::whereMonth('created_at',Carbon::now()->subMonth($i))->count();
+        }
+
+        //Creator Statics
+        $CreatorsThisMonth = User::where('role','creator')->whereMonth('created_at',Carbon::now())->count();
+        $CreatorsLastMonth = User::where('role','creator')->whereMonth('created_at',Carbon::now()->subMonth())->count();
+        for($i=6;$i>=0;$i--){
+            $CreatorsChart[6-$i] = User::where('role','creator')->whereMonth('created_at',Carbon::now()->subMonth($i))->count();
+        }
+
+        //Creator Statics
+        $UsersThisMonth = User::where('role','user')->whereMonth('created_at',Carbon::now())->count();
+        $UsersLastMonth = User::where('role','user')->whereMonth('created_at',Carbon::now()->subMonth())->count();
+        for($i=6;$i>=0;$i--){
+            $UsersChart[6-$i] = User::where('role','user')->whereMonth('created_at',Carbon::now()->subMonth($i))->count();
+        }
+
         return [
-            Stat::make('Unique Posts', Post::query()->count()),
-            Stat::make('Unique Creators', User::where('role','creator')->count()),
-            Stat::make('Unique Users', User::where('role','user')->count()),
+            Stat::make('Posts Count', Post::query()->count())
+                ->description($PostsLastMonth-$PostsThisMonth == 0 ? 'No Change' : abs($PostsLastMonth-$PostsThisMonth) . ($PostsThisMonth >= $PostsLastMonth ? ' increase' : ' decrease'))
+                ->descriptionIcon($PostsLastMonth-$PostsThisMonth == 0 ? 'heroicon-m-minus' : ($PostsThisMonth > $PostsLastMonth ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down'))
+                ->chart($PostsChart)
+                ->color($PostsLastMonth-$PostsThisMonth == 0 ? 'gray' : ($PostsThisMonth > $PostsLastMonth ? 'success' : 'danger')),
+
+            Stat::make('Creators Count', User::where('role','creator')->count())
+                    ->description($CreatorsLastMonth-$CreatorsThisMonth == 0 ? 'No Change' : abs($CreatorsLastMonth-$CreatorsThisMonth) . ($CreatorsThisMonth >= $CreatorsLastMonth ? ' increase' : ' decrease'))
+                    ->descriptionIcon($CreatorsLastMonth-$CreatorsThisMonth == 0 ? 'heroicon-m-minus' : ($CreatorsThisMonth > $CreatorsLastMonth ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down'))
+                    ->chart($CreatorsChart)
+                    ->color($CreatorsLastMonth-$CreatorsThisMonth == 0 ? 'gray' : ($CreatorsThisMonth > $CreatorsLastMonth ? 'success' : 'danger')),
+
+            Stat::make('Users Count', User::where('role','user')->count())
+                    ->description($UsersLastMonth-$UsersThisMonth == 0 ? 'No Change' : abs($UsersLastMonth-$UsersThisMonth) . ($UsersThisMonth >= $UsersLastMonth ? ' increase' : ' decrease'))
+                    ->descriptionIcon($UsersLastMonth-$UsersThisMonth == 0 ? 'heroicon-m-minus' : ($UsersThisMonth > $UsersLastMonth ? 'heroicon-m-arrow-trending-up' : 'heroicon-m-arrow-trending-down'))
+                    ->chart($UsersChart)
+                    ->color($UsersLastMonth-$UsersThisMonth == 0 ? 'gray' : ($UsersThisMonth > $UsersLastMonth ? 'success' : 'danger')),
         ];
+    }
+
+    protected function getHeading(): ?string
+    {
+        return 'Analytics';
+    }
+
+    protected function getDescription(): ?string
+    {
+        return 'An overview of some analytics.';
     }
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,7 +41,6 @@ class AdminPanelProvider extends PanelProvider
             ->profile(MyProfile::class,isSimple: false)
             ->brandName('Blog System')
             ->colors(['primary' => Color::Red])
-            ->userMenu(position: UserMenuPosition::Sidebar)
             ->globalSearch(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
